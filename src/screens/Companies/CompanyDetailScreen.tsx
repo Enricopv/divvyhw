3633a6f6cc33bde145afc79bc9688e9a9c090661
Enrico Valbuena <enricovalbuena@gmail.com>
2022-06-02 21:59:37 -0600

    basic company list and detail

--- src/screens/Companies/CompanyDetailScreen.tsx
+++ src/screens/Companies/CompanyDetailScreen.tsx
@@ -1,9 +1,11 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import * as dateFns from 'date-fns';
 import React from 'react';
 import {
   Dimensions,
   Pressable,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   View,
@@ -35,18 +37,65 @@ const CompanyDetailScreen = ({
 
   return (
     <SafeAreaView style={styles.center}>
-      <View style={styles.header}>
-        <Pressable onPress={onBackPress}>
-          <Text>Back</Text>
-        </Pressable>
-      </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>{companyData?.name}</Text>
-      </View>
-      <View>
-        <CompanyGraph style={styles.graph} data={companyData} />
-      </View>
+      <ScrollView>
+        <View style={styles.header}>
+          <Pressable onPress={onBackPress}>
+            <Text>Back</Text>
+          </Pressable>
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>{companyData?.name}</Text>
+        </View>
+        <View>
+          <CompanyGraph style={styles.graph} data={companyData} />
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <View style={{ width: screenWidth * 0.9 }}>
+            <Text style={styles.revenueTitle}>
+              Revenue From The Past 6 Months
+            </Text>
+
+            <View>
+              {companyData.revenue.map((item, index) => (
+                <RevenueItem
+                  {...item}
+                  isLast={index + 1 >= companyData.revenue.length}
+                  key={index}
+                />
+              ))}
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const RevenueItem = (props: {
+  seq: number;
+  date: string;
+  value: number;
+  isLast: boolean;
+}) => {
+  return (
+    <View
+      style={{
+        ...styles.revenueItem,
+        borderBottomColor: !props.isLast && 'rgba(0,0,0,0.3)',
+        borderBottomWidth: !props.isLast && 1,
+      }}
+      key={props.seq}>
+      <View style={styles.monthText}>
+        <Text style={{ fontSize: 20 }}>
+          {dateFns.format(new Date(props.date.substring(0, 10)), 'MMM')}
+        </Text>
+        <Text style={styles.yearText}>
+          {' '}
+          {dateFns.format(new Date(props.date.substring(0, 10)), 'yy')}
+        </Text>
+      </View>
+      <Text style={{ fontSize: 24 }}>${props.value}</Text>
+    </View>
   );
 };
 
@@ -57,17 +106,38 @@ const styles = StyleSheet.create({
   },
   center: {
     flex: 1,
+    backgroundColor: 'white',
   },
   header: {
     paddingVertical: 12,
     paddingHorizontal: 8,
   },
   title: {
-    fontSize: 22,
+    fontSize: 24,
     fontWeight: 'bold',
   },
   titleContainer: {
     paddingHorizontal: 8,
+  },
+  revenueItem: {
+    flexDirection: 'row',
+    paddingVertical: 8,
+  },
+  revenueTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginVertical: 12,
+    textAlign: 'center',
+  },
+  monthText: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  yearText: {
+    flexGrow: 1,
+    fontSize: 16,
+    color: 'rgba(0,0,0,0.6)',
   },
 });
 
